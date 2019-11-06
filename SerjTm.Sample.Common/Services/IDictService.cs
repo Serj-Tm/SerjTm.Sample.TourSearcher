57f03370f4b386b415da821311a52c7cfe433849
- <->
2019-11-06 14:37:12 +0300

    TourSearcher.Common: contracts added

---
+++ SerjTm.Sample.Common/Services/IDictService.cs
@@ -0,0 +1,26 @@
+﻿using SerjTm.Sample.Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerjTm.Sample.Common.Services
+{
+
+    public interface IDictService
+    {
+        IEnumerable<City> StartCities();
+        IEnumerable<Country> Countries();
+
+        /// <summary>
+        /// Все возможные города, как города вылета, так и города туров
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Country> Cities();
+        /// <summary>
+        /// Поиск отеля по Id
+        /// </summary>
+        /// <param name="id">id отеля</param>
+        /// <returns>null - если не найден</returns>
+        Hotel FindHotel(Guid id);
+    }
+}
---
+++ SerjTm.Sample.Common/Services/ISearchService.cs
@@ -0,0 +1,21 @@
+﻿using SerjTm.Sample.Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerjTm.Sample.Common.Services
+{
+    public enum SearchOrder
+    {
+        byPrice,
+        byPriceDesc,
+        byName,
+        byDate,
+        byDateDesc
+    }
+
+    public interface ISearchService
+    {
+        IEnumerable<Tour> Search(City startCity, City city, DateTime startDate, int minDays, int maxDays, int peopleCount, SearchOrder order);
+    }
+}
