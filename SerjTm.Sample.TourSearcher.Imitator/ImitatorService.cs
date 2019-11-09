9e024803d8441cda82f19119a2570d3a59912d4c
SerjTm <->
2019-11-09 05:01:45 +0300

    TuiProvider, Imitator: data generation moved to Imitator project

---
+++ SerjTm.Sample.TourSearcher.Imitator/ImitatorService.cs
@@ -0,0 +1,65 @@
+﻿using SerjTm.Sample.Common.Model;
+using System;
+using System.Linq;
+
+namespace SerjTm.Sample.TourSearcher.Imitator
+{
+    public class ImitatorService
+    {
+        public static (Country[], City[], Hotel[], Tour[]) Imitate(string provider)
+        {
+            var countries = GenerateCountries();
+            var cities = GenerateCities(countries);
+            var hotels = GenerateHotels(cities);
+            var tours = GenerateTours(provider, cities, hotels);
+
+            return (countries, cities, hotels, tours);
+        }
+
+        static Country[] GenerateCountries() => Enumerable.Range(1, 200).Select(i => new Country(i, $"Country_{i}")).ToArray();
+
+        static City[] GenerateCities(Country[] countries)
+        {
+            var rnd = new Random();
+
+            return Enumerable.Range(1, 1_000)
+                .Select(i => new City(i, countries.ByRandom(rnd), $"City_{i}"))
+                .ToArray();
+        }
+        static Hotel[] GenerateHotels(City[] cities)
+        {
+            var rnd = new Random();
+
+            return Enumerable.Range(1, 1_000)
+                .Select(i => new Hotel(Guid.NewGuid(), $"Hotel_{i}", $"Address_{i}", cities.ByRandom(rnd), rnd.Next(2000, 2019)))
+                .ToArray();
+        }
+        static Tour[] GenerateTours(string provider, City[] cities, Hotel[] hotels)
+        {
+            var rnd = new Random();
+
+            return Enumerable.Range(1, 1_000_000)
+                .Select(i =>
+                {
+                    var days = rnd.Next(1, 30);
+                    var startDay = rnd.Next(0, 50);
+                    var startDate = DateTime.Today.AddDays(startDay);
+                    var startHotelDate = startDate.AddDays(rnd.Next(0, 2));
+                    var endDate = startHotelDate.AddDays(days);
+
+                    var dayPrice = (decimal)(2_000 + rnd.NextDouble() * 10_000);
+
+                    var priceByOnePeople = Math.Round(dayPrice * days, 2);
+
+                    return new Tour(Guid.NewGuid(), provider, hotels.ByRandom(rnd), RoomKind.All.ByRandom(rnd), cities.ByRandom(rnd), startDate, endDate, startHotelDate, days, priceByOnePeople, null, "S7", rnd.Next(1, 6));
+                }
+                )
+                .ToArray();
+        }
+    }
+
+    public static class ArrayHlp
+    {
+        public static T ByRandom<T>(this T[] items, Random rnd) => items[rnd.Next(0, items.Length)];
+    }
+}
--- SerjTm.Sample.TuiProvider/Storages/ImmutableMemoryStorage.cs
+++ SerjTm.Sample.TuiProvider/Storages/ImmutableMemoryStorage.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SerjTm.Sample.TourSearcher.Imitator;
 
 namespace SerjTm.Sample.TuiProvider.Storages
 {
@@ -10,10 +11,7 @@ namespace SerjTm.Sample.TuiProvider.Storages
     {
         public ImmutableMemoryStorage()
         {
-            this.Countries = GenerateCountries();
-            this.Cities = GenerateCities(this.Countries);
-            this.Hotels = GenerateHotels(this.Cities);
-            this.Tours = GenerateTours(this.Cities, this.Hotels);
+            (this.Countries, this.Cities, this.Hotels, this.Tours) = ImitatorService.Imitate("Tui");
         }
 
         public readonly Country[] Countries;
@@ -22,49 +20,7 @@ namespace SerjTm.Sample.TuiProvider.Storages
         public readonly Tour[] Tours;
 
 
-        static Country[] GenerateCountries() => Enumerable.Range(1, 200).Select(i => new Country(i, $"Country_{i}")).ToArray();
-
-        static City[] GenerateCities(Country[] countries)
-        {
-            var rnd = new Random();
-
-            return Enumerable.Range(1, 1_000)
-                .Select(i => new City(i, countries.ByRandom(rnd), $"City_{i}"))
-                .ToArray();
-        }
-        static Hotel[] GenerateHotels(City[] cities)
-        {
-            var rnd = new Random();
-
-            return Enumerable.Range(1, 1_000)
-                .Select(i => new Hotel(Guid.NewGuid(), $"Hotel_{i}", $"Address_{i}", cities.ByRandom(rnd), rnd.Next(2000, 2019)))
-                .ToArray();
-        }
-        static Tour[] GenerateTours(City[] cities, Hotel[] hotels)
-        {
-            var rnd = new Random();
-
-            return Enumerable.Range(1, 1_000_000)
-                .Select(i =>
-                 {
-                     var days = rnd.Next(1, 30);
-                     var startDay = rnd.Next(0, 50);
-                     var startDate = DateTime.Today.AddDays(startDay);
-                     var startHotelDate = startDate.AddDays(rnd.Next(0, 2));
-                     var endDate = startHotelDate.AddDays(days);
-
-                     var dayPrice = (decimal)(2_000 + rnd.NextDouble() * 10_000);
-
-                     var priceByOnePeople = Math.Round(dayPrice * days, 2);
-
-                     return new Tour(Guid.NewGuid(), "Tui", hotels.ByRandom(rnd), RoomKind.All.ByRandom(rnd), cities.ByRandom(rnd), startDate, endDate, startHotelDate, days, priceByOnePeople, null, "S7", rnd.Next(1, 6));
-                 }
-                )
-                .ToArray();
-        }
+ 
     }
-    public static class ArrayHlp
-    {
-        public static T ByRandom<T>(this T[] items, Random rnd) => items[rnd.Next(0, items.Length)];
-    }
+
 }
