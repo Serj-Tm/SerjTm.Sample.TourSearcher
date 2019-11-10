d907b7d59bbfb1e98b90c1324aeb64332f8eb989
SerjTm <->
2019-11-10 21:43:26 +0300

    Aggregator: priorityProvider options added

--- SerjTm.Sample.TourSearcher.Aggregator/AggregatorService.cs
+++ SerjTm.Sample.TourSearcher.Aggregator/AggregatorService.cs
@@ -17,9 +17,14 @@ namespace SerjTm.Sample.TourSearcher.Aggregator
         {
             this.searchers = searchers;
             this.SearchTimeout = TimeSpan.FromSeconds(configuration.GetValue<double>("Aggregator:Search:Timeout", 15));
+
+            var priorityConfig = new PriorityProviderConfig();
+            configuration.GetSection("Aggregator:PriorityProvider").Bind(priorityConfig);
+            this.PriorityProviderConfig = priorityConfig;
         }
         readonly IEnumerable<ISearchService> searchers;
         readonly TimeSpan SearchTimeout;
+        readonly PriorityProviderConfig PriorityProviderConfig;
 
         public async Task<IEnumerable<Tour>> Search(ICity_Id startCity, ICity_Id city, DateTime? startDate, int? minDays, int? maxDays, int? peopleCount, SearchOrder? order, CancellationToken token)
         {
@@ -36,8 +41,26 @@ namespace SerjTm.Sample.TourSearcher.Aggregator
 
             return answers.SelectMany(answer => answer)
                 .GroupBy(tour => new { HotelId = tour.Hotel.Id, StartCityId = tour.StartCity.Id, tour.StartDate, tour.StartHotelDate, tour.Days, tour.RoomKind })
-                .Select(group => group.MinObject(tour => tour.FullPrice))
-                .OrderBy(order);
+                .Select(group => MinPriceTour(group))
+                .OrderBy(order)
+                .ToArray();
         }
+        Tour MinPriceTour(IEnumerable<Tour> tours)
+        {
+            var minTour = tours.MinObject(tour => tour.FullPrice);
+            if (this.PriorityProviderConfig?.Name != null)
+            {
+                var minPriorityTour = tours.Where(tour => tour.Provider == PriorityProviderConfig.Name).MinObject(tour => tour.FullPrice);
+                if (minPriorityTour != null && minPriorityTour.FullPrice <= minTour.FullPrice * (1m + 0.01m * PriorityProviderConfig.ExtraPricePercent))
+                    return minPriorityTour;
+            }
+            return minTour;
+        }
+    }
+
+    public class PriorityProviderConfig
+    {
+        public string Name { get; set; } = null;
+        public decimal ExtraPricePercent { get; set; } = 5;
     }
 }
--- SerjTm.Sample.TourSearcher.Tests/AggregatorTests.cs
+++ SerjTm.Sample.TourSearcher.Tests/AggregatorTests.cs
@@ -16,7 +16,7 @@ namespace SerjTm.Sample.TourSearcher.Tests
     public class AggregatorTests
     {
         [Fact]
-        public async void TestIgnoreSlowSearcher()
+        public async void IgnoreSlowSearcher()
         {
             var config = new Dictionary<string, string>
             {
@@ -30,8 +30,8 @@ namespace SerjTm.Sample.TourSearcher.Tests
             var country = new Country(1, "Russia");
             var city = new City(1, country, "Moscow");
             var hotel = new Hotel(Guid.NewGuid(), "Hilton", "Lenina, 1", city, 2019);
-            var tour1 = new Tour(Guid.NewGuid(), "Tui", hotel, "luxe", city, DateTime.Today, DateTime.Today.AddDays(10), DateTime.Today, 10, 100.0m, null, "S7", 2);
-            var tour2 = new Tour(Guid.NewGuid(), "Tui", hotel, "deluxe", city, DateTime.Today, DateTime.Today.AddDays(10), DateTime.Today, 10, 100.0m, null, "S7", 2);
+            var tour1 = new Tour(Guid.NewGuid(), "Tui", hotel, "luxe", city, DateTime.Today, DateTime.Today.AddDays(10), DateTime.Today, 10, 100.0m, 100.0m, "S7", 2);
+            var tour2 = new Tour(Guid.NewGuid(), "Tui", hotel, "deluxe", city, DateTime.Today, DateTime.Today.AddDays(10), DateTime.Today, 10, 100.0m, 100.0m, "S7", 2);
 
             var cancellation = new CancellationTokenSource().Token;
 
@@ -54,6 +54,53 @@ namespace SerjTm.Sample.TourSearcher.Tests
 
             Assert.Equal(new[] { tour1, tour2 }, result);
             Assert.InRange(watch.Elapsed.TotalSeconds, 0.3, 0.4);
+        }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(20)]
+        public async void PriorityProviderWithExtraPrice(int extraPricePercent)
+        {
+
+            var config = new Dictionary<string, string>
+            {
+                {"Aggregator:PriorityProvider:Name", "Tui"},
+                {"Aggregator:PriorityProvider:ExtraPricePercent", extraPricePercent.ToString()},
+            };
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(config)
+                .Build();
+
+            var country = new Country(1, "Russia");
+            var city = new City(1, country, "Moscow");
+            var hotel = new Hotel(Guid.NewGuid(), "Hilton", "Lenina, 1", city, 2019);
+            var lowExtraPrice = 4m;
+            var tour_other_lowExtraPrice = new Tour(Guid.NewGuid(), "Other", hotel, "luxe", city, DateTime.Today, DateTime.Today.AddDays(10), DateTime.Today, 10, 100.0m, 100.0m, "S7", 2);
+            var tour_tui_lowExtraPrice = tour_other_lowExtraPrice.With(provider:"Tui", fullPrice: tour_other_lowExtraPrice.FullPrice * (1 + 0.01m*lowExtraPrice));
+            var highExtraPrice = 10m;
+            var tour_other_highExtraPrice = new Tour(Guid.NewGuid(), "Other", hotel, "deluxe", city, DateTime.Today, DateTime.Today.AddDays(10), DateTime.Today, 10, 100.0m, 100.0m, "S7", 2);
+            var tour_tui_highExtraPrice = tour_other_highExtraPrice.With(provider:"Tui", fullPrice: tour_other_highExtraPrice.FullPrice * (1 +  0.01m * highExtraPrice));
+
+            var cancellation = new CancellationTokenSource().Token;
+
+            var searcher1 = new Mock<ISearchService>();
+            searcher1.Setup(mock => mock.Search(null, null, null, null, null, null, null, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new[] { tour_other_lowExtraPrice, tour_other_highExtraPrice });
+            var searcher2 = new Mock<ISearchService>();
+            searcher2.Setup(mock => mock.Search(null, null, null, null, null, null, null, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new[] { tour_tui_lowExtraPrice, tour_tui_highExtraPrice });
+
+            var aggregator = new AggregatorService(new[] { searcher1.Object, searcher2.Object}, configuration);
+
+            var result = await aggregator.Search(null, null, null, null, null, null, null, cancellation);
+
+            Assert.Equal(new[] 
+                {
+                  lowExtraPrice < extraPricePercent ?  tour_tui_lowExtraPrice : tour_other_lowExtraPrice,
+                  highExtraPrice < extraPricePercent ?  tour_tui_highExtraPrice : tour_other_highExtraPrice
+                }, 
+                result);
         }
     }
 }
